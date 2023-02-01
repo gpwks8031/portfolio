3977658f4916227848653f890a9f77dfe3240502
gpwks8031 <gpwls8031@naver.com>
2023-02-01 15:43:42 +0900

    1차 수정

---
+++ js/index.js
@@ -0,0 +1,182 @@
+// gnb 메뉴 누르면 해당 파트로 이동
+const gnbA = document.querySelectorAll(".gnb li a");
+const sections = document.querySelectorAll("section");
+gnbA.forEach((gnb, idx) => {
+  gnb.addEventListener("click", (e) => {
+    e.preventDefault();
+    const getBox = sections[idx].offsetTop;
+    scrollTo({
+      top: getBox,
+      behavior: "smooth",
+    });
+  });
+});
+
+// gnb 스크롤 내리면 사라지고, 올리면 나타남
+let lastScrollTop = 0;
+let gnb = document.querySelector(".gnb");
+window.addEventListener("scroll", () => {
+  let scrollTop = window.pageXOffset || document.documentElement.scrollTop;
+  if (scrollTop > lastScrollTop) {
+    gnb.style.top = "-70px";
+  } else {
+    gnb.style.top = "0";
+  }
+  lastScrollTop = scrollTop;
+});
+
+// title 이름 밑줄
+const myName = document.querySelector("strong");
+const once = setTimeout(() => {
+  myName.classList.add("active");
+}, 2000);
+
+// 슬라이드
+const slider = document.querySelector(".slider_inner");
+const progressBar = document.querySelector(".progress_bar_inner");
+
+let sliderGrabbed = false;
+
+slider.parentElement.addEventListener("scroll", (e) => {
+  console.log(getScrollPercentage());
+  progressBar.style.width = `${getScrollPercentage()}%`;
+});
+slider.addEventListener("mousedown", (e) => {
+  sliderGrabbed = true;
+  slider.style.cursor = "grabbing";
+});
+slider.addEventListener("mouseup", (e) => {
+  sliderGrabbed = false;
+  slider.style.cursor = "grab";
+});
+slider.addEventListener("mouseleave", (e) => {
+  sliderGrabbed = false;
+  slider.style.cursor = "grab";
+});
+slider.addEventListener("mousemove", (e) => {
+  if (sliderGrabbed) {
+    slider.parentElement.scrollLeft -= e.movementX;
+  }
+});
+// slider.addEventListener("wheel", (e) => {
+//   e.preventDefault();
+//   slider.parentElement.scrollLeft += e.delayY;
+// });
+function getScrollPercentage() {
+  return (
+    (slider.parentElement.scrollLeft /
+      (slider.parentElement.scrollWidth - slider.parentElement.clientWidth)) *
+    100
+  );
+}
+
+// skills skill_list 각각 애니메이션
+const ani1 = document.querySelector(".skill_list .ani1");
+const ani2 = document.querySelector(".skill_list .ani2");
+const ani3 = document.querySelector(".skill_list .ani3");
+const ani4 = document.querySelector(".skill_list .ani4");
+const ani5 = document.querySelector(".skill_list .ani5");
+addEventListener("scroll", () => {
+  let value = window.scrollY;
+  if (value > 1200) {
+    ani1.style.animation = "mov 0.6s ease";
+    ani2.style.animation = "mov 0.75s ease";
+    ani3.style.animation = "mov 0.8s ease";
+    ani4.style.animation = "mov 0.95s ease";
+    ani5.style.animation = "mov 1.5s ease";
+  }
+});
+
+// about text 타이핑
+let text =
+  "꾸준히 노력하고, 포기하지 않으며, 도전정신이 있는 프론트엔드 개발자";
+let i = 0;
+const about = document.querySelector(".about");
+window.addEventListener("scroll", () => {
+  const getAbout = about.offsetTop;
+  const scrollY = window.scrollY;
+  // console.log(getAbout);
+  if (scrollY >= getAbout) {
+    // function resetTyping() {
+    //   document.getElementById("text").innerHTML = "";
+    // }
+    function typing() {
+      if (i < text.length) {
+        document.getElementById("text").innerHTML += text.charAt(i);
+        i++;
+        setTimeout(typing, 50);
+      } else if ((i) => text.length) {
+        clearTimeout(typing);
+      }
+    }
+    typing();
+  }
+});
+
+// go_up about에 고정 + 스르륵 누르면 올라감
+const goUp = document.querySelector(".go_up");
+window.addEventListener("scroll", () => {
+  const scrollY = window.scrollY;
+  const getAbout = sections[3].offsetTop;
+  if (scrollY >= getAbout) {
+    goUp.style.opacity = 1;
+  } else {
+    goUp.style.opacity = 0;
+  }
+});
+goUp.addEventListener("click", (e) => {
+  e.preventDefault();
+  scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
+
+// dots 누르면 색깔,모양 변경
+const dots = document.querySelectorAll(".dots li");
+dots.forEach((item) => {
+  item.addEventListener("click", function (e) {
+    e.preventDefault();
+    for (let i = 0; i < dots.length; i++) {
+      dots[i].classList.remove("on");
+    }
+    this.classList.add("on");
+  });
+});
+
+// dots 누르면 해당 파트 이동
+dots.forEach((dot, idx) => {
+  dot.addEventListener("click", (e) => {
+    e.preventDefault();
+    const getBox = sections[idx].offsetTop;
+    scrollTo({
+      top: getBox,
+      behavior: "smooth",
+    });
+  });
+});
+
+// dots 스크롤에 따른 각 색깔,모양 변경
+window.addEventListener("scroll", () => {
+  dots.forEach((dot, idx) => {
+    const getSection = sections[idx].offsetTop - 100;
+    const scrollY = window.scrollY;
+    if (scrollY >= getSection) {
+      dots.forEach((dot) => {
+        dot.classList.remove("on");
+      });
+      dots[idx].classList.add("on");
+    }
+  });
+});
+
+// 마지막 dot 노랑색으로 변경
+window.addEventListener("scroll", () => {
+  const getAbout = sections[3].offsetTop - 300;
+  const scrollY = window.scrollY;
+  if (scrollY >= getAbout) {
+    dots[3].style.backgroundColor = "yellow";
+  } else {
+    dots[3].style.backgroundColor = "#d9d9d9";
+  }
+});
